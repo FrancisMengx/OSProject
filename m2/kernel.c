622ce492042cea333cc954d37f5630cade1098e2
Tianyu Liu <lty1993@gmail.com>
2015-01-18 21:49:46 -0500

    readString

--- m2/kernel.c
+++ m2/kernel.c
@@ -5,14 +5,35 @@
 
 
 void printString(char *chars);
+void readString(char *chars);
 
 int main() {
+	//interrupt(0x10, 0xe * 256 + 'L', 0, 0, 0);
+	//interrupt(0x10, 0xe * 256 + 'T', 0, 0, 0);
+	//interrupt(0x10, 0xe * 256 + 'Y', 0, 0, 0);
+	char line[80];
+	printString("Enter a line: \0");
+	readString(line);
+	printString(line);
+}
 
-	printString("test\0");
+void readString(char *chars)	{
+	int i;
+	char tmp;
+	i = 0;
+	while(1) {
+		tmp = interrupt(0x16, 0, 0, 0, 0);
+		if(tmp == 0xd) {
+			chars[i] = 0xa;
+			chars[i+1] = 0x0;
+			break;
+		}
+		chars[i] = tmp;
+		i++;
+	}
 }
 
 void printString(char *chars) {
-	
 	int i;
 	char ah;
 	int ax;
