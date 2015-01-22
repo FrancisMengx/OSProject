8ab6643fd1c21f2ae3c75818b045336a90244074
Francis Meng <meng.francis@gmail.com>
2015-01-21 22:14:07 -0500

    not done yet

---
+++ m3/kernel.c
@@ -0,0 +1,149 @@
+/*
+	Francis Meng, Noah Miller, Tianyu Liu
+	Group 1-H
+*/
+
+
+void printString(char *chars);
+void handleInterrupt21(int ax, int bx, int cx, int dx);
+void readFile (char *fileName, char buffer[]);
+void readFileList();
+void readSector(char *buffer, int sector);
+void readString(char *chars);
+void setCursor(int pos);
+int mod(int a, int b);
+int div(int a, int b);
+
+int main() {
+	char line[512];
+	int tmp;
+	char buffer[512];
+	printString("Enter a line: \0");
+	readString(line);
+	printString(line);
+  readSector(buffer, 30);
+  printString(buffer);
+  makeInterrupt21();
+  interrupt(0x21, 1, line, 30, 0);
+  interrupt(0x21, 0, line, 0, 0);
+  while(1){asm "hlt";}
+}
+
+void readFile (char *fileName, char buffer[]){
+
+}
+
+void readFileList (){
+  char sectorBuffer[512];
+  int i;
+  int j;
+  //read director sector
+  char sector[16][2][26];
+  readSector(sectorBuffer, 2);
+  for(i = 0; i < 512; i++){
+    sector[div(i, 32)][0][j] = 
+  }
+}
+
+void readSector(char *buffer, int sector){
+  int rs;
+  int head;
+  int track;
+  int ah;
+  int al;
+  int ch;
+  int cl;
+  int dh;
+  int dl;
+
+  rs = mod(sector, 18) + 1;
+  head = mod(div(sector, 18), 2);
+  track = div(sector, 36);
+  ah = 2;
+  al = 1;
+  ch = track;
+  cl = rs;
+  dh = head;
+  dl = 0;
+  interrupt(0x13, ah*256+al, buffer, ch*256+cl, dh*256+dl);
+
+}
+
+void readString(char *chars)	{
+	int i;
+	char tmp;
+  char tmpArr[2];
+	i = 0;
+	while(1) {
+		tmp = interrupt(0x16, 0, 0, 0, 0);
+		if(tmp == 0xd) {
+      interrupt(0x10, 0xe*256+0xa, 0, 0, 0);
+      interrupt(0x10, 0xe*256+0xd, 0, 0, 0);
+			chars[i] = 0xa;
+			chars[i+1] = 0x0;
+			break;
+		}
+    if(tmp == 0x8){
+      if(i > 0){
+        interrupt(0x10, 0xe*256+8, 0, 0, 0);
+        chars[i-1] = 0x0;
+        i--;
+        interrupt(0x10, 0xe*256, 0, 0, 0);
+        interrupt(0x10, 0xe*256+8, 0, 0, 0);
+        continue;
+      }
+      continue;
+    }
+		chars[i] = tmp;
+    interrupt(0x10, 0xe*256+tmp, 0, 0, 0);
+		i++;
+	}
+}
+
+void printString(char *chars) {
+	int i;
+	char ah;
+	int ax;
+	i = 0;
+	ah = 0xe;
+	while (chars[i] != '\0') {
+		ax = ah * 256 + chars[i];
+		interrupt(0x10, ax, 0, 0, 0);
+		i++;
+    if(chars[i] == '\n'){
+      interrupt(0x10, 0xe*256+0xd, 0, 0, 0);
+    }
+  }
+}
+
+
+void handleInterrupt21(int ax, int bx, int cx, int dx) {
+	switch(ax) {
+		case 0: 
+			printString((char *)bx);
+			break;
+		case 1: 
+			readString((char *)bx);
+			break;
+		case 2:
+			readSector((char *)bx, cx);
+			break;
+		default: 
+			printString("Error ax");
+			break;
+	}
+}
+
+int mod(int a, int b){
+  while(a >= b)
+      a = a - b;
+  return a;
+}
+
+int div(int a, int b){
+  int quo = 0;
+  while((quo + 1)*b <=a){
+    quo++;
+  }
+  return quo;
+}
